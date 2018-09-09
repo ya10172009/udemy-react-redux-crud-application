e67e54a01d9c08c8427360afe80dd48821ea399b
tepsys <tepsys@MacBookFujihime.local>
2018-09-10 01:29:12 +0900

    jsxの学習

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//           Hello, World!!
+//       </div>
+//     );
+//   }
+// }
 
 class App extends Component {
   render() {
     return (
-      <div>
-          <h1 >Hello World!!</h1>
-      </div>
+      <h1>
+        Hello, World!!!!
+      </h1>
     );
+
   }
 }
+
 
 export default App;
